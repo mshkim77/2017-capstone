3d8aeae3034deabcfbfd9f36da3734a5ae588e62
Jun Hyeok Jung <junny@junjung.me>
2017-08-12 12:18:01 +0900

    requirements.txt 파일 추가. 오디오 모델 약간 수정.

--- audioManager/admin.py
+++ audioManager/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import AudioFile
 
 class AudioFileAdmin(admin.ModelAdmin):
-    list_display = ('file_id', 'original_file', 'upload_date')
-
+    list_display = ('file_id', 'file', 'converted_file', 'upload_date')
 
 admin.site.register(AudioFile, AudioFileAdmin)
