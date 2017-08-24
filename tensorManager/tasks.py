3e446bc1de48378281b117da0276d7761f351474
Jun Hyeok Jung <junny@junjung.me>
2017-08-24 18:36:19 +0900

    메인페이지 연결 (audio/new) HTML Reset CSS 추가 일부 이미지 변경 오디오 녹음 페이지에서 파일이 재생중일 때 녹음 버튼을 클릭하지 못하도록 잠금 설정 텐서플로우 테스크 관련 코드 추가

--- tensorManager/tasks.py
+++ tensorManager/tasks.py
@@ -1,2 +1,8 @@
-import tensorflow as tf
 from capstone2017.celery import app as celery_app
+
+import tensorflow as tf
+import numpy as np
+
+
+def learn_emotion_from_words(words, emotions):
+    print("Hello!")
